72a65cb8e20301dfe50b785a03f30193a87ee7a2
clamp1981 <clamp1981@gmail.com>
2022-05-26 22:55:36 +0900

    feat : Add QuizeQuestionActivity and when the start button click, satrt the QuizeQuestionActivity

--- app/src/main/java/com/example/myquizapp/MainActivity.kt
+++ app/src/main/java/com/example/myquizapp/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.myquizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart : Button = findViewById(R.id.btnStart)
+        val txtName : EditText = findViewById(R.id.txtName )
+
+        btnStart.setOnClickListener{
+            if( !txtName?.text.isNotEmpty() ) {
+                Toast.makeText(this,
+                    "Please Enter your name!", Toast.LENGTH_LONG).show()
+            }else{
+                val intent = Intent( this, QuizeQuestionActivity::class.java)
+                startActivity(intent)
+                finish()
+
+            }
+
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/myquizapp/QuizeQuestionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myquizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizeQuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quize_question)
+    }
+}
