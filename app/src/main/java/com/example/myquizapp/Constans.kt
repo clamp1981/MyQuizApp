32a8f7b453b48f08363da3d0c133b2333a8d38a6
clamp1981 <clamp1981@gmail.com>
2022-05-27 21:07:21 +0900

    feat : add function getQuestion

--- app/src/main/java/com/example/myquizapp/Constans.kt
+++ app/src/main/java/com/example/myquizapp/Constans.kt
@@ -1,4 +1,61 @@
 package com.example.myquizapp
 
 object Constans {
+
+    fun getQuestions() : ArrayList<Question>{
+        val questionString = "What country does this flag belong to?"
+        var questions = ArrayList<Question>()
+        var qur01 = Question (
+            1,
+            questionString,
+            R.drawable.ic_flag_of_argentina,
+            "America",
+            "South Korea",
+            "Japan",
+            "Argaentina",
+            3 )
+
+        questions.add(qur01)
+
+        var qur02 = Question (
+            2,
+            questionString,
+            R.drawable.ic_flag_of_australia,
+            "South Korea",
+            "Australia",
+            "French",
+            "America",
+            3 )
+
+        questions.add(qur02)
+
+        var qur03 = Question (
+            3,
+            questionString,
+            R.drawable.ic_flag_of_belgium,
+            "French",
+            "Australia",
+            "Belgium",
+            "America",
+            2 )
+
+        questions.add(qur03)
+
+        var qur04 = Question (
+            4,
+            questionString,
+            R.drawable.ic_flag_of_denmark,
+            "French",
+            "Australia",
+            "Belgium",
+            "Denmark",
+            3 )
+
+        questions.add(qur04)
+
+
+
+        return questions
+
+    }
 }
